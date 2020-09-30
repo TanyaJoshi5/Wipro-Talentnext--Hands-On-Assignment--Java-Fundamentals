70a1e4ef771c2a192df382bc7d91ce4e72d2de9d
TanyaJoshi5 <54098287+TanyaJoshi5@users.noreply.github.com>
2020-10-01 00:02:00 +0530

    Create Language Basics-1.java

---
+++ Language Basics-1.java
@@ -0,0 +1,9 @@
+class LanguageBasics1
+{
+  public static void main(String[] args)
+  {
+    String s1=args[0];
+    String s2=args[1];
+    System.out.print(args[0]+"Technologies"+args[1]);
+  }
+}
